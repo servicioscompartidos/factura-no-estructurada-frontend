9836af0fa430d9de63e0b87b3a12c454b87eb2ca
Luis Alejandro Samuel Banegas <lsamuel@servicioscompartidos.com>
2026-01-20 16:25:01 -0600

    creacion de modelo

---
+++ src/app/Shared/Models/home.model.ts
@@ -0,0 +1,11 @@
+export interface Supplier{
+    accgasstationid: number
+    accountcode_code: string
+    cif: string
+    city: string
+    country: string
+    gasstationid: number
+    postalcode: string
+    state: string
+    suppliercompanyname: string
+}
