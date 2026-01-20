bc9e48a906f1740facbc6d6e3ea0c49be5e2028d
Luis Alejandro Samuel Banegas <lsamuel@servicioscompartidos.com>
2026-01-20 16:25:12 -0600

    variables de entorno

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: false,
     apiAuth: 'https://gtapiopdw.endtoend.com.mx/api/v1/servicios-compartidos/emisor-retenciones',
-    apiURL:'http://192.168.88.102:3000'
+    // apiURL:'http://10.253.7.241:10020',
+    // apiURL:'http://localhost:10020',
+    apiURL:'https://gtapirfpc.endtoend.com.mx',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiAuth: 'https://gtapiopdw.endtoend.com.mx/api/v1/servicios-compartidos/emisor-retenciones',
-    apiUrl: 'localhost:3000'
+    apiURL:'https://gtapirfpc.endtoend.com.mx',
+    // apiURL:'http://10.253.7.241:10020',
 };
